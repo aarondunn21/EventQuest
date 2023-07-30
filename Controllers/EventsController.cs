e7ac2227575077140321168fd485339357345ec2
aarondunn21 <105383281+aarondunn21@users.noreply.github.com>
2023-07-30 07:50:54 -0400

    Add project files.

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EventQuest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+public class EventsController : Controller
+{
+    private readonly string apiKey = "qOGkxRUqBIhxhbcLDxtysEz4XrspA9bp";
+    private readonly string apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json";
+
+    public async Task<ActionResult> Index()
+    {
+        try
+        {
+            // Set up the HttpClient
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Set up the query parameters
+                string countryCode = "US"; // Change to your desired country code
+                string city = "Atlanta"; // Change to your desired city
+
+                // Build the API request URL with query parameters and API key
+                string requestUrl = $"{apiUrl}?apikey={apiKey}&countryCode={countryCode}&city={city}";
+
+                // Send the HTTP request and get the response
+                HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+
+                // Check if the request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the response content as a string
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    // Deserialize the JSON response into a class representing the Ticketmaster API response
+                    TicketmasterResponse ticketmasterResponse = JsonConvert.DeserializeObject<TicketmasterResponse>(jsonResponse);
+
+                    // Pass the events data to the view
+                    return View(ticketmasterResponse._embedded.events);
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = $"Failed to fetch events. Status Code: {response.StatusCode}";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            ViewBag.ErrorMessage = $"Error: {ex.Message}";
+        }
+
+        // Return an empty view if an error occurred
+        return View();
+    }
+}
+
---
+++ Models/TicketmasterEvent.cs
@@ -0,0 +1,55 @@
+﻿namespace EventQuest.Models
+{
+    public class TicketmasterResponse
+    {
+        public Embedded _embedded { get; set; }
+    }
+
+    public class Embedded
+    {
+        public Event[] events { get; set; }
+    }
+
+    public class Event
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public Dates dates { get; set; }
+        public EmbeddedVenue _embedded { get; set; }
+        public Images[] images { get; set; }
+    }
+
+    public class Dates
+    {
+        public Start start { get; set; }
+    }
+
+    public class Start
+    {
+        public string localDate { get; set; }
+        public string localTime { get; set; }
+    }
+
+    public class EmbeddedVenue
+    {
+        public Venue[] venues { get; set; }
+    }
+
+    public class Venue
+    {
+        public string name { get; set; }
+    }
+
+/*    public class EmbeddedImage
+    {
+        public Image[] images { get; set; }
+    }
+*/
+    public class Images
+    {
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+}
